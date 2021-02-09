40739aa1ca8add6bb1629a57809177a755abfdef
ishitachauhan <ishitachauhan073@gmail.com>
2021-02-09 17:24:11 +0530

    please review

--- index.js
+++ index.js
@@ -1,41 +1,24 @@
-
+const urls = require("./server.json")
 const request = require('request')
-let urls = [
-    {
-    "url": "http://doesNotExist.kratikal.com",
-    "priority": 1
-    },
-    {
-    "url": "http://kratikal.com",
-    "priority": 7
-    },
-    {
-    "url": "http://offline.kratikal.com",
-    "priority": 2
-    },
-    {
-    "url": "http://google.com",
-    "priority": 4
-    }
-    ]
-for (var i=0;i<urls.length;i++){
-    var server_url = urls[i].url;
-    var priority_rank = urls[i].priority;
-    
-    const myPromise = new Promise(function(resolve, reject) {
-        request
-        .get(server_url,function (error,response){
-            console.log('error:' ,error);
-            console.log('statusCode:',response && response.statusCode);
-    
-    });
-        setTimeout(function() {
-            resolve('Promise returns after 5 second!');    
-        }, 5000);
-    });
-    myPromise
-    .then(function (values) {
-        console.log(values)
-        return values
-    })
-}
+const axios = require('axios')
+    async function findServer() {
+
+        const tasks = urls.map(source => axios.get(source.url,{timeout: 5000}));
+        const results = await Promise.allSettled(tasks);
+        results.forEach((result) => {
+            if (result.status === 'fulfilled') {
+              const { value } = result;
+                if(value.status >= 200 && value.status <= 299){
+                    for (i=0;i<urls.length;i++){
+                        console.log(urls[i].priority)
+                    }
+                }
+            } else { // results.status === 'rejected'
+              const { reason } = result
+              // do something...
+            }
+          });
+        }
+
+       
+       findServer();
